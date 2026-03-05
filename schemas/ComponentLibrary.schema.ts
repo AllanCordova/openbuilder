98957fad72f3e9d161f7b443e0d87998da7142aa
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2026-03-05 19:43:15 -0300

    chore: add global constants, validation schemas, and setup scripts

---
+++ schemas/ComponentLibrary.schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const getComponentsSchema = z.object({
+  page: z.number().optional().default(1),
+  limit: z.number().optional().default(10),
+  tag: z.string().optional(),
+});
+
+export type GetComponentsSchema = z.infer<typeof getComponentsSchema>;
